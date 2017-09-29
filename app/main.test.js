63062d29126f9c880d2b7de6a26a3175bdb97e67
enconstant <enconstant@gmail.com>
2017-09-29 17:06:18 +0300

    added tests on click events

--- app/main.test.js
+++ app/main.test.js
@@ -70,3 +70,91 @@ test('draw function result match', () => {
       '<span>test_val</span>' +
       '</p>');
 });
+//	It seems that there must be a better way to test click events
+//	JQuery was used to simulate click event 
+test('check classes replaced on one click', () => {
+  document.body.innerHTML =
+    '<div class="page__rendering-block">' +
+    '<div class="object-render"></div>' +
+    '</div>';
+  const main = require('./main');
+  const $ = require('jquery');
+  $('.object-render').eq(0).html('');
+  $('.object-render').eq(0).html(main.drawObject(obj2, main.draw));
+  $('.object-render__key-line').eq(0).click();
+  expect($('.object-render__key-line').eq(0).next()
+    .hasClass('object-render__object-block_collapsed')).toBeTruthy;
+
+  expect($('.object-render__key-line').eq(0).next()
+    .hasClass('object-render__object-block')).toBeFalsy;
+
+  expect($('.object-render__key-line').eq(0).next().next()
+    .hasClass('object-render__bracket')).toBeTruthy;
+
+  expect($('.object-render__key-line').eq(0).next().next()
+    .hasClass('object-render__bracket_hidden')).toBeFalsy;
+
+  expect($('.object-render__key-line').eq(0).first()
+    .hasClass('object-render__key-name_extended')).toBeTruthy;
+
+  expect($('.object-render__key-line').eq(0).first()
+    .hasClass('object-render__key-name_collapsed')).toBeFalsy;
+});
+
+test('check classes replaced on two clicks', () => {
+  document.body.innerHTML =
+    '<div class="page__rendering-block">' +
+    '<div class="object-render"></div>' +
+    '</div>';
+  const main = require('./main');
+  const $ = require('jquery');
+  $('.object-render').eq(0).html('');
+  $('.object-render').eq(0).html(main.drawObject(obj2, main.draw));
+  $('.object-render__key-line').eq(0).click().click();
+  expect($('.object-render__key-line').eq(0).next()
+    .hasClass('object-render__object-block_collapsed')).toBeFalsy;
+
+  expect($('.object-render__key-line').eq(0).next()
+    .hasClass('object-render__object-block')).toBeTruthy;
+
+  expect($('.object-render__key-line').eq(0).next().next()
+    .hasClass('object-render__bracket')).toBeFalsy;
+  
+  expect($('.object-render__key-line').eq(0).next().next()
+    .hasClass('object-render__bracket_hidden')).toBeTruthy;
+
+  expect($('.object-render__key-line').eq(0).first()
+    .hasClass('object-render__key-name_extended')).toBeFalsy;
+
+  expect($('.object-render__key-line').eq(0).first()
+    .hasClass('object-render__key-name_collapsed')).toBeTruthy;
+});
+
+test('check classes replaced when clicked on span inside p', () => {
+  document.body.innerHTML =
+    '<div class="page__rendering-block">' +
+    '<div class="object-render"></div>' +
+    '</div>';
+  const main = require('./main');
+  const $ = require('jquery');
+  $('.object-render').eq(0).html('');
+  $('.object-render').eq(0).html(main.drawObject(obj2, main.draw));
+  $('.object-render__key-name_extended').eq(0).click();
+  expect($('.object-render__key-line').eq(0).next()
+    .hasClass('object-render__object-block_collapsed')).toBeTruthy;
+
+  expect($('.object-render__key-line').eq(0).next()
+    .hasClass('object-render__object-block')).toBeFalsy;
+
+  expect($('.object-render__key-line').eq(0).next().next()
+    .hasClass('object-render__bracket')).toBeTruthy;
+
+  expect($('.object-render__key-line').eq(0).next().next()
+    .hasClass('object-render__bracket_hidden')).toBeFalsy;
+
+  expect($('.object-render__key-line').eq(0).first()
+    .hasClass('object-render__key-name_extended')).toBeTruthy;
+
+  expect($('.object-render__key-line').eq(0).first()
+    .hasClass('object-render__key-name_collapsed')).toBeFalsy;
+});
